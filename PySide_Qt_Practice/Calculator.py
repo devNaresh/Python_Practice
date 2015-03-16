c289c20e3b31655e937c4232a7985c412141babb
Naresh <lnresh60@ymail.com>
2015-03-16 15:12:32 +0530

    Added Calculator.py file

---
+++ PySide_Qt_Practice/Calculator.py
@@ -0,0 +1,31 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+import sys
+
+class Form(QDialog):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.browser = QTextBrowser()
+        self.linedit = QLineEdit("Type Here and Press Enter")
+        self.linedit.selectAll()
+
+        widget = QVBoxLayout()
+        widget.addWidget(self.browser)
+        widget.addWidget(self.linedit)
+        self.setLayout(widget)
+        self.setWindowTitle('Calculator')
+        self.linedit.setFocus()
+
+        self.linedit.returnPressed.connect(self.uichange)
+
+    def uichange(self):
+        try:
+            text = self.linedit.text()
+            self.browser.append('%s = <b>%s</b>' % (text, eval(text)))
+        except:
+            self.browser.append('Enter valid expression')
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
