3bbc8d989ce4d98e7ca6825215bd513cf5b139de
nareshCredr <naresh.k@credr.com>
2016-08-14 22:48:34 +0530

    added docstring in asyncio.py

--- async_io.py
+++ async_io.py
@@ -7,13 +7,20 @@ import sys
 
 @asyncio.coroutine
 def spin(msg):
+    """
+    To cancel execution of spin we need to give control back
+    to event loop. 'yield' or 'yield from' statement will give control
+    back to event loop.
+
+    """
     write, flush = sys.stdout.write, sys.stdout.flush
     for char in itertools.cycle('|/-\\'):
-        status = char + ' ' + msg
-        print(status)
-        write('\x08' * len(status))
         try:
+            status = char + ' ' + msg
+            print(status)
+            write('\x08' * len(status))
             yield from asyncio.sleep(.1)
+            # yield
         except asyncio.CancelledError:
             break
     write(' ' * len(status) + '\x08' * len(status))
@@ -22,16 +29,25 @@ def spin(msg):
 @asyncio.coroutine
 def slow_function():
     # pretend waiting a long time for I/O
-    yield from asyncio.sleep(3)
+    yield from asyncio.sleep(2)
     return 42
 
 
 @asyncio.coroutine
 def supervisor():
+    """
+    In this we can call spin() with 'yield from' as it is co-routine but
+    the problem is you cannot control it from outside so we created a Task
+
+    """
+
     spinner = asyncio.ensure_future(spin('thinking!'))
+    # spinner = yield from spin('thinking!')
     print('spinner object:', spinner)
     result = yield from slow_function()
+    print("*"*50)
     spinner.cancel()
+    print('spinner object:', spinner)
     return result
 
 
